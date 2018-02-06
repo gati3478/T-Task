cf2ecc2f3a6f9f664bd96428127dc40bae201098
gati3478 <giopetriashvili@gmail.com>
2018-02-06 11:29:05 +0400

    Improvements to gradle build script plugin resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,24 +5,20 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "com.task"
 version = "0.0.3-SNAPSHOT"
 
-val kotlinVersion: String = "1.2.21"
-
 plugins {
-	val kotlinVersion = "1.2.21"
 	val springBootVersion = "1.5.10.RELEASE"
-	id("java")
-	id("org.jetbrains.kotlin.jvm") version kotlinVersion
-	id("groovy")
-	id("idea")
+	java
+	groovy
+	id("org.jetbrains.kotlin.jvm")
 	id("org.springframework.boot") version springBootVersion
-	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
-	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+	id("org.jetbrains.kotlin.plugin.allopen")
+	id("org.jetbrains.kotlin.plugin.spring")
+	id("org.jetbrains.kotlin.plugin.jpa")
 }
 
-configure<JavaPluginConvention> {
-	setSourceCompatibility(1.8)
-	setTargetCompatibility(1.8)
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
@@ -34,27 +30,34 @@ tasks {
 }
 
 repositories {
-	mavenCentral()
+	jcenter()
 }
 
 dependencies {
-	compile("org.springframework.boot:spring-boot-starter-web")
-	compile("org.springframework.boot:spring-boot-starter-security")
-	compile("org.springframework.boot:spring-boot-starter-data-jpa")
-	compile("org.springframework.boot:spring-boot-starter-thymeleaf")
-	compile("org.thymeleaf.extras:thymeleaf-extras-springsecurity4")
-	compile("com.h2database:h2")
-	compile("javax.xml.bind:jaxb-api:2.3.0")
-	compile(kotlin("stdlib-jdk8", version = kotlinVersion))
-	compile(kotlin("reflect", version = kotlinVersion))
-	compile("org.codehaus.groovy:groovy-all:2.4.13")
+	listOf(
+		"org.springframework.boot:spring-boot-starter-web",
+		"org.springframework.boot:spring-boot-starter-security",
+		"org.springframework.boot:spring-boot-starter-data-jpa",
+		"org.springframework.boot:spring-boot-starter-thymeleaf",
+		"org.thymeleaf.extras:thymeleaf-extras-springsecurity4",
+		"com.h2database:h2",
+		"javax.xml.bind:jaxb-api:2.3.0",
+		kotlin("stdlib-jdk8"),
+		kotlin("reflect"),
+		"org.codehaus.groovy:groovy-all:2.4.13"
+	).forEach { implementation(it) }
 
 	// Testing
-	testCompile("org.spockframework:spock-core:1.1-groovy-2.4")
-	testCompile("org.spockframework:spock-spring:1.1-groovy-2.4")
-	testCompile("org.hamcrest:hamcrest-core:1.3")
-	testRuntime("net.bytebuddy:byte-buddy:1.7.10")
-	testRuntime("org.objenesis:objenesis:2.6")
-	testCompile("org.springframework.boot:spring-boot-starter-test")
-	testCompile("org.springframework.security:spring-security-test")
+	listOf(
+		"org.spockframework:spock-core:1.1-groovy-2.4",
+		"org.spockframework:spock-spring:1.1-groovy-2.4",
+		"org.hamcrest:hamcrest-core:1.3",
+		"org.springframework.boot:spring-boot-starter-test",
+		"org.springframework.security:spring-security-test"
+	).forEach { testImplementation(it) }
+
+	listOf(
+		"net.bytebuddy:byte-buddy:1.7.10",
+		"org.objenesis:objenesis:2.6"
+	).forEach { testRuntime(it) }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,14 @@ rootProject.name = "t-task"
 
 pluginManagement {
 	repositories {
-		mavenCentral()
+		jcenter()
 		gradlePluginPortal()
+	}
+	resolutionStrategy {
+		eachPlugin {
+			if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+				useVersion(gradle.rootProject.extra["kotlin.version"] as String)
+			}
+		}
 	}
 }
